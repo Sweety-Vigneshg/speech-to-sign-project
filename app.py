ef4762d9ca282c85c2519dfd526a7576fb2e9c69
Sweety-Vigneshg <kuttydravid463@gmail.com>
2025-03-30 20:29:56 +0530

    File delete

--- app.py
+++ app.py
@@ -479,4 +479,4 @@ def admin():
     return render_template("admin.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True, host='0.0.0.0', port=10000)
+    app.run(debug=True, threaded=True)
